e200ac8278bb883f9440fba1ecd9a67d9e0c2f8c
Takashi Hashida <hashida@clear-code.com>
2026-01-20 16:59:16 +0900

    Implement refrection config data to a reply mail

--- src/web/app.js
+++ src/web/app.js
@@ -13,62 +13,54 @@ Office.onReady(() => {});
 
 async function onTypicalReplyButtonClicked(event) {
   const actionId = event.source.id; 
-  console.log(actionId);
-  console.log("conversationId: " + Office.context.mailbox.item.conversationId);
-  const config = await ConfigLoader.loadConfigForCurrentLanguage(Office.context.displayLanguage);
+  console.debug("actionId: " + actionId);
+  console.debug("conversationId: " + Office.context.mailbox.item.conversationId);
   const originalMailData = {
     toRecipients: Office.context.mailbox.item.to,
     ccRecipients: Office.context.mailbox.item.cc,
     bccRecipients: Office.context.mailbox.item.bcc,
     sender: Office.context.mailbox.item.sender,
-    body: Office.context.mailbox.item.body,
     subject: Office.context.mailbox.item.subject,
     id: Office.context.mailbox.item.itemId,
   };
-  const abc = await OfficeDataAccessHelper.getAllMailData();
-  // Office.context.mailbox.item.displayReplyAllFormAsync({
-  //   htmlBody:
-  // });
-  console.log(Office.context.mailbox.item.itemId);
   try {
-    const waitComplete = false;
-    for(const buttonConfig of config.ButtonConfigList) {
-      if (actionId !== buttonConfig.Id) {
-        continue;
-      }
-      const replyMailData = MailDataCreator.CreateDataOnForReplyForm({ config: config.ButtonConfigList[0], originalMailData });
-      if (!replyMailData) {
-        return event.completed();
-      }
-      Office.context.roamingSettings.set(targetConversationId, Office.context.mailbox.item.conversationId ?? "");
-      Office.context.roamingSettings.set("buttonConfig", JSON.stringify(config.ButtonConfigList[0]));
-      Office.context.roamingSettings.saveAsync();
-      replyMailData.executeMethod({attachments: replyMailData.attachments});
-      //Office.context.mailbox.displayNewMessageFormAsync(replyMailData,);
-      // displayNewMessageFormAsync will be canceled if event.completed() is called
-      // before finishing displayNewMessageFormAsync. The event will be completed 
-      // automatically after displayNewMessageFormAsync is called.
-      waitComplete = true;
-      break;
+    const buttonConfig = await ConfigLoader.loadConfigForCurrentLanguageAndButtonId(Office.context.displayLanguage, actionId);
+    if (!buttonConfig) {
+      console.log("no button config find.")
+      return event.completed();
     }
-    event.completed();
+    const replyMailData = MailDataCreator.CreateDataOnForReplyForm({ config: buttonConfig, originalMailData });
+    if (!replyMailData) {
+      console.log("failed to create reply mail data.")
+      return event.completed();
+    }
+    Office.context.roamingSettings.set("conversationId", Office.context.mailbox.item.conversationId ?? "");
+    Office.context.roamingSettings.set("actionId", actionId);
+    await OfficeDataAccessHelper.saveRoamingSettingsAsync();
+    replyMailData.executeMethod({attachments: replyMailData.attachments, callback: () => {
+      event.completed();
+    }});
   } catch (e) {
     console.log("createNewMail Failed:", e);
+    event.completed();
   }
 }
 window.onTypicalReplyButtonClicked = onTypicalReplyButtonClicked;
 
 async function onNewMessageComposeCreated(event) {
-  const id =  Office.context.mailbox.item.conversationId;
-  const targetConversationId = Office.context.roamingSettings.get(conversationId)?.trim() ?? "";
-  if (id !== targetConversationId) {
-    event.completed();
+  const conversationId =  Office.context.mailbox.item.conversationId;
+  const actionId = Office.context.roamingSettings.get("actionId")?.trim() ?? "";
+  const targetConversationId = Office.context.roamingSettings.get("conversationId")?.trim() ?? "";
+  console.debug("action id: " + actionId);
+  console.debug("targetConversation id: " + targetConversationId);
+  if (conversationId !== targetConversationId) {
+    return event.completed();
   }
-  const buttonConfigJson = Office.context.roamingSettings.get("buttonConfig")?.trim() ?? "";
-  Office.context.roamingSettings.remove(conversationId);
-  Office.context.roamingSettings.remove("buttonConfig");
-  Office.context.roamingSettings.saveAsync();
-  const buttonConfig = JSON.parse(buttonConfigJson);
+  const buttonConfig = await ConfigLoader.loadConfigForCurrentLanguageAndButtonId(Office.context.displayLanguage, actionId);
+  Office.context.roamingSettings.remove("conversationId");
+  Office.context.roamingSettings.remove("actionId");
+  await OfficeDataAccessHelper.saveRoamingSettingsAsync();
+
   const currentSubject = await OfficeDataAccessHelper.getSubjectAsync();
   const data = MailDataCreator.CreateReplyMailData({config: buttonConfig, currentSubject});
   if (data.newToRecipients) {
